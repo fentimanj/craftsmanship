acfc2b6a7c3a83500427e4152f9a0733aad02c90
johnfentiman <john.fentiman@icloud.com>
2026-06-03 09:04:14 +0100

    R

--- Craftsmanship/LessonOne/LeapYearKata/src/Services/LeapYearService.cs
+++ Craftsmanship/LessonOne/LeapYearKata/src/Services/LeapYearService.cs
@@ -4,20 +4,11 @@ public class LeapYearService
 {
     public bool IsLeapYear(int year)
     {
-        if (year == 1900)
+        if (year.IsDivisibleBy100())
         {
             return false;
         }   
-        
-        if (year == 1800)
-        {
-            return false;
-        } 
-        
-        if (year == 2100)
-        {
-            return false;
-        }
+      
         
         if (year.IsDivisibleByFour())
         {
@@ -38,5 +29,10 @@ internal static class LeapYearServiceExtensions
     public static bool IsDivisibleByFour(this int year)
     {
         return year % 4 == 0; 
+    }
+    
+    public static bool IsDivisibleBy100(this int year)
+    {
+        return year % 100 == 0;
     }
 }
