afd576813198dda14b40de4f084502a521437955
johnfentiman <john.fentiman@icloud.com>
2026-06-04 10:21:17 +0100

    Solved

---
+++ Craftsmanship/LessonOne/NthFibonacci/src/Services/FibonacciService.cs
@@ -0,0 +1,17 @@
+namespace src.Services;
+
+public static class FibonacciService
+{
+    public static int ConvertNumber(this int input)
+    {
+        var conversionNMinus1 = input - 1;
+        var conversionNMinus2 = input - 2;
+        
+        return input switch
+        {
+            0 => 0,
+            1 => 1,
+            _ => conversionNMinus1.ConvertNumber() + conversionNMinus2.ConvertNumber()
+        };
+    }
+}
--- Craftsmanship/LessonOne/NthFibonacci/tests/FibonacciServiceShould.cs
+++ Craftsmanship/LessonOne/NthFibonacci/tests/FibonacciServiceShould.cs
@@ -1,35 +1,25 @@
 namespace tests;
 
 using FluentAssertions;
+using src.Services;
 
 public class FibonacciServiceShould
 {
     [Theory]
     [InlineData(0, 0)]
     [InlineData(1, 1)]
-    [InlineData(2, 2)]
+    [InlineData(2, 1)]
+    [InlineData(3, 2)]
+    [InlineData(4, 3)]
+    [InlineData(5, 5)]
+    [InlineData(6, 8)]
+    [InlineData(7, 13)]
+    [InlineData(8, 21)]
+    [InlineData(9, 34)]
+    [InlineData(10, 55)]
     public void ReturnCorrectConvertedNumber_WhenConvertNumberInvoked_GivenValidInput(int input, int expectedConversion)
     {
-        FibonacciService fibonacciService = new FibonacciService();
-        int fibonacciNumber = fibonacciService.ConvertNumber(input);
+        var fibonacciNumber = input.ConvertNumber();
         fibonacciNumber.Should().Be(expectedConversion);
-    }
-}
-
-public class FibonacciService
-{
-    public int ConvertNumber(int input)
-    {
-        if (input == 1)
-        {
-            return 1;
-        }
-
-        if (input == 2)
-        {
-            return 2;
-        }
-        
-        return 0;
     }
 }
