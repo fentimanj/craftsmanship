3738a3fa7f1efb473922186f9580749e3983701e
johnfentiman <john.fentiman@icloud.com>
2026-06-10 20:20:01 +0100

    R

--- Craftsmanship/CodeWars/CogsebisGearNetwork/src/Cogsebis.cs
+++ Craftsmanship/CodeWars/CogsebisGearNetwork/src/Cogsebis.cs
@@ -1,24 +1,10 @@
-using System;
-
 public class Cogsebi
 {
     public static double[] CalculateRpms(int[] gears, (int, int)[] connections, int driverId, double driverRpm)
     {
-        if (gears.Length == 2 && gears[1] == 10)
-        {
-            return [driverRpm, -100.0];
-        }
-        
-        if (gears.Length == 2 && gears[1] == 20)
-        {
-            return [driverRpm, -50.0];
-        }
-        
-        if (gears.Length == 2 && gears[1] == 40)
-        {
-            return [driverRpm, -25.0];
-        }
-        
-        return [driverRpm];
+        if (gears.Length == 1) return [driverRpm];
+
+        var rpm = driverRpm * (gears[0] / (double)gears[1]);
+        return [driverRpm, -rpm];
     }
 }
