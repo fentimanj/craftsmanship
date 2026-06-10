70ae2a055d0c54baa3ddc1c67dd1049a2bbed537
johnfentiman <john.fentiman@icloud.com>
2026-06-10 13:45:16 +0100

    R

--- Craftsmanship/CodeWars/CogsebisGearNetwork/src/Cogsebis.cs
+++ Craftsmanship/CodeWars/CogsebisGearNetwork/src/Cogsebis.cs
@@ -4,17 +4,6 @@ public class Cogsebi
 {
     public static double[] CalculateRpms(int[] gears, (int, int)[] connections, int driverId, double driverRpm)
     {
-        if (driverRpm == 200.0)
-        {
-            return [200];
-        }
-        
-        if (driverRpm == 300.0)
-        {
-            return [300];
-        }
-
-        
-        return [100];
+        return [driverRpm];
     }
 }
--- Craftsmanship/CodeWars/CogsebisGearNetwork/tests/CogsebiShould.cs
+++ Craftsmanship/CodeWars/CogsebisGearNetwork/tests/CogsebiShould.cs
@@ -4,44 +4,23 @@ using FluentAssertions;
 
 public class CogsebiShould
 {
-    [Fact]
-    public void ReturnCorrectRpms_WhenCalculateRpmsInvoked_GivenOnlyOneGearWith100Rpm()
+    [Theory]
+    [InlineData(100.0, 100.0)]
+    [InlineData(200.0, 200.0)]
+    [InlineData(300.0, 300.0)]
+    public void ReturnCorrectRpms_WhenCalculateRpmsInvoked_GivenOnlyOneGear(double driverRpm, double expected)
     {
         int[] gears = [10];
         (int, int)[] connections = [];
         const int driverId = 0;
-        const double driverRpm = 100;
         
         var rpmOfCog = Cogsebi.CalculateRpms(gears, connections, driverId, driverRpm);
         
-        rpmOfCog.First().Should().Be(100.0);
-    }    
-    
-    [Fact]
-    public void ReturnCorrectRpms_WhenCalculateRpmsInvoked_GivenOnlyOneGearWith200Rpm()
-    {
-        int[] gears = [10];
-        (int, int)[] connections = [];
-        const int driverId = 0;
-        const double driverRpm = 200;
-        
-        var rpmOfCog = Cogsebi.CalculateRpms(gears, connections, driverId, driverRpm);
-        
-        rpmOfCog.First().Should().Be(200.0);
-    } 
-    
-    [Fact]
-    public void ReturnCorrectRpms_WhenCalculateRpmsInvoked_GivenOnlyOneGearWith300Rpm()
-    {
-        int[] gears = [10];
-        (int, int)[] connections = [];
-        const int driverId = 0;
-        const double driverRpm = 300;
-        
-        var rpmOfCog = Cogsebi.CalculateRpms(gears, connections, driverId, driverRpm);
-        
-        rpmOfCog.First().Should().Be(300.0);
+        rpmOfCog.First().Should().Be(expected);
     }
+    
+    [Fact]
+    
 }
 
 
