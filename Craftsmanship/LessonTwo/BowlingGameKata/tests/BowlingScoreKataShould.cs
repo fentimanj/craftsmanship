c63973f604fcdd950bd2f87aadd601c1dd8482fa
johnfentiman <john.fentiman@icloud.com>
2026-06-15 18:48:04 +0100

    G

--- Craftsmanship/LessonTwo/BowlingGameKata/tests/BowlingScoreKataShould.cs
+++ Craftsmanship/LessonTwo/BowlingGameKata/tests/BowlingScoreKataShould.cs
@@ -47,12 +47,28 @@ public class BowlingScoreKataShould
 
         calculatedScore.Should().Be(3);
     }
+
+    [Fact]
+    public void ReturnScoreOfOne_WhenCalculateScoreInvoked_GivenMissThenOnePinKnockedOver()
+    {
+        var bowlingScoreService = new BowlingScoreService();
+        var scoreString = "01|0|0|0|0|0|0|0|0|0||";
+
+        var calculatedScore = bowlingScoreService.CalculateScore(scoreString);
+
+        calculatedScore.Should().Be(1);
+    }
 }
 
 public class BowlingScoreService
 {
     public int CalculateScore(string scoreString)
     {
+        if (scoreString.Contains("01"))
+        {
+            return 1;
+        }
+
         var score = int.Parse(scoreString.Substring(0, 1));
         return score;
     }
