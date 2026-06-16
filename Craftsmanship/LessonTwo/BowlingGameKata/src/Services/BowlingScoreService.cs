2dca8494c5c9909cf0b738c5bd2aa05f6fcde4f7
johnfentiman <john.fentiman@icloud.com>
2026-06-16 16:02:02 +0100

    r

--- Craftsmanship/LessonTwo/BowlingGameKata/src/Services/BowlingScoreService.cs
+++ Craftsmanship/LessonTwo/BowlingGameKata/src/Services/BowlingScoreService.cs
@@ -4,11 +4,11 @@ public class BowlingScoreService
 {
     public int CalculateScore(string scoreString)
     {
-        if (scoreString.Contains("01")) return 1;
+        if (scoreString.Contains("01")) return int.Parse($"{scoreString[1]}");
 
-        if (scoreString.Contains("02")) return 2;
+        if (scoreString.Contains("02")) return int.Parse($"{scoreString[1]}");
 
-        if (scoreString.Contains("03")) return 3;
+        if (scoreString.Contains("03")) return int.Parse($"{scoreString[1]}");
 
         var score = int.Parse(scoreString.Substring(0, 1));
         return score;
