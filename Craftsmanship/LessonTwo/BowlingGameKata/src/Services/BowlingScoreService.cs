06c17bf2844be30a5cbf6e661209e7d1dd532607
johnfentiman <john.fentiman@icloud.com>
2026-06-16 16:34:52 +0100

    R

--- Craftsmanship/LessonTwo/BowlingGameKata/src/Services/BowlingScoreService.cs
+++ Craftsmanship/LessonTwo/BowlingGameKata/src/Services/BowlingScoreService.cs
@@ -5,14 +5,16 @@ public class BowlingScoreService
     public int CalculateScore(string scoreString)
     {
         var cleansedScoreString = scoreString.Replace("-", "0");
+        var splitScoreCardString = scoreString.Split("|");
 
-        if (scoreString == "03|02|0|0|0|0|0|0|0|0||")
+        var firstSetAsString = $"{splitScoreCardString[0]}";
+        var secondSetAsString = $"{splitScoreCardString[1]}";
+
+        var totalScore = 0;
+        
+        
+        if (firstSetAsString.Length == 2 && secondSetAsString.Length == 2)
         {
-            var splitScoreCardString = scoreString.Split("|");
-            
-            var firstSetAsString = $"{splitScoreCardString[0]}";
-            var secondSetAsString = $"{splitScoreCardString[1]}";
-            
             var firstSetFirstBallPins = int.Parse($"{firstSetAsString[0]}");
             var firstSetSecondBallPins = int.Parse($"{firstSetAsString[1]}");
             var secondSetFirstBallPins = int.Parse($"{secondSetAsString[0]}");
@@ -20,20 +22,10 @@ public class BowlingScoreService
 
             var firstSet = firstSetFirstBallPins + firstSetSecondBallPins;
             var secondSet = secondSetFirstBallPins + secondSetSecondBallPins;
-            
+
             return firstSet + secondSet;
-        }    
-        
-        if (scoreString == "03|03|0|0|0|0|0|0|0|0||")
-        {
-            return (0 + 3) + (0 + 3);
         }
-        
-        if (scoreString == "04|03|0|0|0|0|0|0|0|0||")
-        {
-            return (0 + 4) + (0 + 3);
-        }
-        
+
         if (cleansedScoreString[1] != '|')
         {
             var firstBallPinsAsString = $"{cleansedScoreString[0]}";
