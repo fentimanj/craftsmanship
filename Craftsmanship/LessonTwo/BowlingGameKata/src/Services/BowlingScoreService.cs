7002e543d015dfdc109fc6c70064259416d298f5
johnfentiman <john.fentiman@icloud.com>
2026-06-16 16:11:46 +0100

    R

--- Craftsmanship/LessonTwo/BowlingGameKata/src/Services/BowlingScoreService.cs
+++ Craftsmanship/LessonTwo/BowlingGameKata/src/Services/BowlingScoreService.cs
@@ -4,19 +4,15 @@ public class BowlingScoreService
 {
     public int CalculateScore(string scoreString)
     {
-        if (scoreString[1] == '|')
+        var cleansedScoreString = scoreString.Replace("-", "0");
+     
+        if (cleansedScoreString[1] != '|')
         {
-            return 0;
-        }
-        
-        if (scoreString[1] != '|' && scoreString[1] != '-')
-        {
-            var firstBallPinsAsString = $"{scoreString[0]}";
-            var secondBallPinsAsString = $"{scoreString[1]}";
+            var firstBallPinsAsString = $"{cleansedScoreString[0]}";
+            var secondBallPinsAsString = $"{cleansedScoreString[1]}";
             return int.Parse(firstBallPinsAsString) + int.Parse(secondBallPinsAsString);
         }
 
-        var score = int.Parse(scoreString.Substring(0, 1));
-        return score;
+        return 0;
     }
 }
