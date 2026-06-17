630d56430ef1d792d824f23e2d477efcd267257a
John Fentiman <john.fentiman@capitalontap.com>
2026-06-17 15:55:30 +0100

    R

--- Craftsmanship/CodeWars/KnightVsRook/src/KnightRock.cs
+++ Craftsmanship/CodeWars/KnightVsRook/src/KnightRock.cs
@@ -2,23 +2,34 @@ using System;
 
 public class KnightRook
 {
-    public static string KnightVsRook(object[] knightPosition, object[]rookPosition)
+    public static string KnightVsRook(object[] rawknightPosition, object[]rawrookPosition)
     {
-        var rookNumberRow = (int)rookPosition[0];
-        var rookLetterRow = (int)((string)rookPosition[1])[0];
-        var knightNumberRow = (int)knightPosition[0];
-        var knightLetterRow = (int)((string)knightPosition[1])[0];
+        var rookPosition = new Position(rawrookPosition);
+
+        var knightLetterRow = (int)((string)rawknightPosition[1])[0];
+        var knightPosition = new Position(rawknightPosition);
         
-        if((rookNumberRow == (knightNumberRow - 2)|| rookNumberRow == (knightNumberRow + 2) )&& (rookLetterRow == (knightLetterRow + 1) || rookLetterRow == (knightLetterRow - 1) ) )
+        if((rookPosition.NumberRow == (knightPosition.NumberRow - 2)|| rookPosition.NumberRow == (knightPosition.NumberRow + 2) )&& (rookPosition.LetterRow == (knightLetterRow + 1) || rookPosition.LetterRow == (knightPosition.LetterRow - 1) ) )
         {
             return "Knight";
         }
         
-        if (rookNumberRow == knightNumberRow || rookLetterRow == knightLetterRow)
+        if (rookPosition.NumberRow == knightPosition.NumberRow || rookPosition.LetterRow == knightPosition.LetterRow)
         {
             return "Rook";
         }
 
         return "None";
     }
+
+    private static bool InReachOfRook(int rookNumberRow, int knightNumberRow, int rookLetterRow, int knightLetterRow)
+    {
+        return rookNumberRow == knightNumberRow || rookLetterRow == knightLetterRow;
+    }
+}
+
+public class Position(object[] rawPosition)
+{
+    public int NumberRow { get; } = (int)rawPosition[0];
+    public int LetterRow { get; } = ((string)rawPosition[1])[0];
 }
