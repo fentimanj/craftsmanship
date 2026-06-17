cb910c1b44e4f176b7948bca3c4c07283b15bb69
johnfentiman <john.fentiman@icloud.com>
2026-06-17 17:46:48 +0100

    G

--- Craftsmanship/LessonThree/RomanNumbersKata/tests/RomanNumbersServiceShould.cs
+++ Craftsmanship/LessonThree/RomanNumbersKata/tests/RomanNumbersServiceShould.cs
@@ -1,6 +1,25 @@
 namespace tests;
 
+using FluentAssertions;
+
 public class RomanNumbersServiceShould
 {
-    
+    [Fact]
+    public void ReturnCorrectNumber()
+    {
+        RomanNumbersService romanNumbersService = new RomanNumbersService();
+        int inputNumber = 1;
+        
+        string expectedRomanNumeral = romanNumbersService.Convert(inputNumber);
+        
+        expectedRomanNumeral.Should().Be("I");
+    }
+}
+
+public class RomanNumbersService
+{
+    public string Convert(int inputNumber)
+    {
+        return "I";
+    }
 }
