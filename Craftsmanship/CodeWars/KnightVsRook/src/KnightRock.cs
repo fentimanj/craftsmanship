921bf10d1112e5a1ade700b43e2aa628989e64c8
John Fentiman <john.fentiman@capitalontap.com>
2026-06-17 15:04:06 +0100

    R

--- Craftsmanship/CodeWars/KnightVsRook/src/KnightRock.cs
+++ Craftsmanship/CodeWars/KnightVsRook/src/KnightRock.cs
@@ -5,13 +5,13 @@ public class KnightRook
     public static string KnightVsRook(object[] knightPosition, object[]rookPosition)
     {
         var rookNumberRow = (int)rookPosition[0];
-        var rookLetterRow = ((string)rookPosition[1])[0];
+        var rookLetterRow = (int)((string)rookPosition[1])[0];
         var knightNumberRow = (int)knightPosition[0];
-        var knightLetterRow = ((string)knightPosition[1])[0];
+        var knightLetterRow = (int)((string)knightPosition[1])[0];
 
 
         
-        if(rookNumberRow == (knightNumberRow - 2) && rookLetterRow == 'D' && knightNumberRow == 8 && knightLetterRow == 'C')
+        if(rookNumberRow == (knightNumberRow - 2) && rookLetterRow == (knightLetterRow + 1) && knightNumberRow == 8 && knightLetterRow == 'C')
         {
             return "Knight";
         }
