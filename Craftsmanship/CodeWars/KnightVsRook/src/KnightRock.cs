24aaffda4e59b807b8a22ae2ca840a1e03c0d0c8
John Fentiman <john.fentiman@capitalontap.com>
2026-06-17 15:38:04 +0100

    G

--- Craftsmanship/CodeWars/KnightVsRook/src/KnightRock.cs
+++ Craftsmanship/CodeWars/KnightVsRook/src/KnightRock.cs
@@ -8,20 +8,8 @@ public class KnightRook
         var rookLetterRow = (int)((string)rookPosition[1])[0];
         var knightNumberRow = (int)knightPosition[0];
         var knightLetterRow = (int)((string)knightPosition[1])[0];
-
-
         
-        if(rookNumberRow == (knightNumberRow - 2) && (rookLetterRow == (knightLetterRow + 1) || rookLetterRow == (knightLetterRow - 1) ) )
-        {
-            return "Knight";
-        }
-        
-        if(rookNumberRow == (knightNumberRow - 2)  && rookLetterRow == (knightLetterRow - 1))
-        {
-            return "Knight";
-        }
-        
-        if(rookNumberRow == (knightNumberRow + 2) && rookLetterRow == 'B' && knightNumberRow == 8 && knightLetterRow == 'C')
+        if((rookNumberRow == (knightNumberRow - 2)|| rookNumberRow == (knightNumberRow + 2) )&& (rookLetterRow == (knightLetterRow + 1) || rookLetterRow == (knightLetterRow - 1) ) )
         {
             return "Knight";
         }
@@ -30,8 +18,7 @@ public class KnightRook
         {
             return "Rook";
         }
-        // Three possible outputs are "Knight", "Rook" and "None".
-        // Happy Coding
+
         return "None";
     }
 }
