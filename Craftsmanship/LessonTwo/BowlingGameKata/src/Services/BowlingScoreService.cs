94bb7119f7bc9b2a72e49c3a3450379caf9cb0da
johnfentiman <john.fentiman@icloud.com>
2026-06-17 09:01:14 +0100

    R

--- Craftsmanship/LessonTwo/BowlingGameKata/src/Services/BowlingScoreService.cs
+++ Craftsmanship/LessonTwo/BowlingGameKata/src/Services/BowlingScoreService.cs
@@ -6,23 +6,23 @@ public class BowlingScoreService
 {
     public static int CalculateScore(string rawScoreCard)
     {
-       var splitScoreCardString = rawScoreCard.Replace(" ", "").Split("|");
+        var splitScoreCardString = rawScoreCard.Replace(" ", "").Split("|");
 
-       var firstSetWasSpare = splitScoreCardString[0] == "1/";
-       
-       if (firstSetWasSpare && splitScoreCardString[1] == "1-")
+        var firstSetWasSpare = splitScoreCardString[0] == "1/";
+
+        if (firstSetWasSpare && splitScoreCardString[1] == "1-")
         {
-            return (1 + 9 + 1) + (1 + 0) ;
-        } 
-        
+            return 1 + 9 + 1 + 1 + 0;
+        }
+
         if (firstSetWasSpare && splitScoreCardString[1] == "2-")
         {
-            return (1 + 9 + 2) + (2 + 0) ;
+            return 1 + 9 + 2 + 2 + 0;
         }
-        
+
         if (firstSetWasSpare && splitScoreCardString[1] == "8-")
         {
-            return (1 + 9 + 8) + (8 + 0) ;
+            return 1 + 9 + 8 + 8 + 0;
         }
 
         var scoringSets = splitScoreCardString.MapSets();
