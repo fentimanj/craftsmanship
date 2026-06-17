b7e0073304951a15834b7d33a76b68e8b5bceb01
John Fentiman <john.fentiman@capitalontap.com>
2026-06-17 14:58:32 +0100

    R

--- Craftsmanship/CodeWars/KnightVsRook/src/KnightRock.cs
+++ Craftsmanship/CodeWars/KnightVsRook/src/KnightRock.cs
@@ -5,21 +5,23 @@ public class KnightRook
     public static string KnightVsRook(object[] knightPosition, object[]rookPosition)
     {
         var rookNumberRow = (int)rookPosition[0];
-        var rookLetterRow = (string)rookPosition[1];
+        var rookLetterRow = ((string)rookPosition[1])[0];
         var knightNumberRow = (int)knightPosition[0];
-        var knightLetterRow = (string)knightPosition[1];
+        var knightLetterRow = ((string)knightPosition[1])[0];
+
+
         
-        if(rookNumberRow == (knightNumberRow - 2) && rookLetterRow == "D" && knightNumberRow == 8 && knightLetterRow == "C")
+        if(rookNumberRow == (knightNumberRow - 2) && rookLetterRow == 'D' && knightNumberRow == 8 && knightLetterRow == 'C')
         {
             return "Knight";
         }
         
-        if(rookNumberRow == (knightNumberRow - 2)  && rookLetterRow == "B" && knightNumberRow == 8 && knightLetterRow == "C")
+        if(rookNumberRow == (knightNumberRow - 2)  && rookLetterRow == 'B' && knightNumberRow == 8 && knightLetterRow == 'C')
         {
             return "Knight";
         }
         
-        if(rookNumberRow == (knightNumberRow + 2) && rookLetterRow == "B" && knightNumberRow == 8 && knightLetterRow == "C")
+        if(rookNumberRow == (knightNumberRow + 2) && rookLetterRow == 'B' && knightNumberRow == 8 && knightLetterRow == 'C')
         {
             return "Knight";
         }
