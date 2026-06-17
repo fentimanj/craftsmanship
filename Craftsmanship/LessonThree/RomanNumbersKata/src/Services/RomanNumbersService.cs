432426eef3f6c17d11076be90011d83505a31c11
johnfentiman <john.fentiman@icloud.com>
2026-06-17 18:23:32 +0100

    R

--- Craftsmanship/LessonThree/RomanNumbersKata/src/Services/RomanNumbersService.cs
+++ Craftsmanship/LessonThree/RomanNumbersKata/src/Services/RomanNumbersService.cs
@@ -6,29 +6,12 @@ public class RomanNumbersService
     {
         var output = string.Empty;
         
-        if (inputNumber == 5)
+        if (inputNumber >= 5)
         {
             output = "V";
             inputNumber -= 5;
         }
-
-        if (inputNumber == 6)
-        {
-            output = "VI";
-            inputNumber -= 6;
-        }
-
-        if (inputNumber == 7)
-        {
-            output = "VII";
-            inputNumber -= 7;
-        }
-
-        if (inputNumber > 4)
-        {
-            return output;
-        }
-
+        
         for (var i = 0; i < inputNumber; i++)
         {
             output += "I";
