996a6aac3372e7f2230e134e6cfb9c4dcfae09ca
johnfentiman <john.fentiman@icloud.com>
2026-07-01 19:10:09 +0100

    G

--- Craftsmanship/CodeWars/BreakCamelCase/src/Services/Kata.cs
+++ Craftsmanship/CodeWars/BreakCamelCase/src/Services/Kata.cs
@@ -4,35 +4,26 @@ public class Kata
 {
     public static string BreakCamelCase(string str)
     {
-        if (str == "helloWorld")
+        if (str.ToLower() != str)
         {
-            var findIndex = 0;
+            var indexOfStartOfSecondWord = 0;
 
             for (var i = 0; i < str.Length; i++)
             {
                 if(str[i].ToString().ToUpper() == str[i].ToString())
                 {
-                    findIndex = i;
+                    indexOfStartOfSecondWord = i;
                     break;
                 }
             }
             
-            var indexOfStartOfSecondWord = findIndex;
             var firstWord = str.Substring(0, indexOfStartOfSecondWord);
             var secondWord = str.Substring(indexOfStartOfSecondWord);
             
             return firstWord + " " + secondWord;
         }  
         
-        if (str == "niceDay")
-        {
-            return "nice Day";
-        }  
         
-        if (str == "badPlan")
-        {
-            return "bad Plan";
-        }
 
         return str;
     }
