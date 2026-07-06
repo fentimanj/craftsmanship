849dd07cc4a69e3b2532bd32f0751768567cc84d
johnfentiman <john.fentiman@icloud.com>
2026-07-06 21:12:26 +0100

    R

--- Craftsmanship/LessonFour/LessonFour/src/Services/TicTacToeGame.cs
+++ Craftsmanship/LessonFour/LessonFour/src/Services/TicTacToeGame.cs
@@ -4,9 +4,9 @@ using Enums;
 
 public class TicTacToeGame
 {
-    private bool isXSymbolNext = true;
     private readonly List<Move> moves = new();
-    
+    private bool isXSymbolNext = true;
+
     public char NextSymbolIs()
     {
         return this.isXSymbolNext ? 'X' : 'O';
@@ -14,48 +14,72 @@ public class TicTacToeGame
 
     public void TakeTurn(Column column, Row row)
     {
-        var move = new Move(this.NextSymbolIs(), column,row);
+        var move = new Move(this.NextSymbolIs(), column, row);
         this.moves.Add(move);
         this.isXSymbolNext = !this.isXSymbolNext;
     }
 
     public string WinnerIs()
     {
+        if (this.WeHaveAColumn() != '-')
+        {
+            return this.WeHaveAColumn().ToString();
+        }
         if (this.ThereAreThreeInColumn('O', Column.Left) ||
             this.ThereAreThreeInColumn('O', Column.Centre) ||
             this.ThereAreThreeInColumn('O', Column.Right))
         {
             return "O";
         }
-        
+
         if (this.ThereAreThreeInColumn('X', Column.Left) ||
             this.ThereAreThreeInColumn('X', Column.Centre) ||
             this.ThereAreThreeInColumn('X', Column.Right))
         {
             return "X";
         }
-        
+
         if (this.ThereAreThreeInRow('X', Row.Top))
         {
             return "X";
         }
-        
+
         return "Unknown";
+    }
+
+    private char WeHaveAColumn()
+    {
+        var xInLeftColumn = this.moves.Count(move => move.Column == Column.Left && move.Symbol == 'X') == 3;
+        var oInLeftColumn = this.moves.Count(move => move.Column == Column.Left && move.Symbol == 'O') == 3;
+
+        var xInCentreColumn = this.moves.Count(move => move.Column == Column.Centre && move.Symbol == 'X') == 3;
+        var oInCentreColumn = this.moves.Count(move => move.Column == Column.Centre && move.Symbol == 'O') == 3;
+
+        var xInRightColumn = this.moves.Count(move => move.Column == Column.Right && move.Symbol == 'X') == 3;
+        var oInRightColumn = this.moves.Count(move => move.Column == Column.Right && move.Symbol == 'O') == 3;
+
+        if (xInLeftColumn || xInCentreColumn || xInRightColumn)
+        {
+            return 'X';
+        }
+
+        if (oInLeftColumn || oInCentreColumn || oInRightColumn)
+        {
+            return 'O';
+        }
+
+        return '-';
     }
 
     private bool ThereAreThreeInColumn(char symbol, Column column)
     {
         return this.moves.Count(move => move.Column == column && move.Symbol == symbol) == 3;
-    } 
-    
+    }
+
     private bool ThereAreThreeInRow(char symbol, Row row)
     {
         return this.moves.Count(move => move.Row == row && move.Symbol == symbol) == 3;
     }
-    
-    
-    
-    
 }
 
 public record Move(char Symbol, Column Column, Row Row);
