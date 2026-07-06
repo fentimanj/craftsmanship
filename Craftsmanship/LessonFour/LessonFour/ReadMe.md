a3194c7ab6a524017517de0031872400959e73fb
johnfentiman <john.fentiman@icloud.com>
2026-07-06 18:12:30 +0100

    Setup for lesson 4

---
+++ Craftsmanship/LessonFour/LessonFour/ReadMe.md
@@ -0,0 +1,3 @@
+# LessonFour
+
+LessonFour
---
+++ Craftsmanship/LessonFour/LessonFour/tests/LessonFourShould.cs
@@ -0,0 +1,8 @@
+public class LessonFourShould
+{
+    [Fact]
+    public void Return_When_Given()
+    {
+        throw new NotImplementedException();
+    }
+}
