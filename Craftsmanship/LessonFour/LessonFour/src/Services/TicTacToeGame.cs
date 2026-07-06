8f2b521eb32bf570584570fecddb361c2f6aa4d5
johnfentiman <john.fentiman@icloud.com>
2026-07-06 21:12:49 +0100

    R

--- Craftsmanship/LessonFour/LessonFour/src/Services/TicTacToeGame.cs
+++ Craftsmanship/LessonFour/LessonFour/src/Services/TicTacToeGame.cs
@@ -25,19 +25,6 @@ public class TicTacToeGame
         {
             return this.WeHaveAColumn().ToString();
         }
-        if (this.ThereAreThreeInColumn('O', Column.Left) ||
-            this.ThereAreThreeInColumn('O', Column.Centre) ||
-            this.ThereAreThreeInColumn('O', Column.Right))
-        {
-            return "O";
-        }
-
-        if (this.ThereAreThreeInColumn('X', Column.Left) ||
-            this.ThereAreThreeInColumn('X', Column.Centre) ||
-            this.ThereAreThreeInColumn('X', Column.Right))
-        {
-            return "X";
-        }
 
         if (this.ThereAreThreeInRow('X', Row.Top))
         {
