0687ede973b6ae6021da20f1b0992c2db7888c89
John Fentiman <john.fentiman@capitalontap.com>
2026-07-08 13:48:15 +0100

    G

---
+++ Craftsmanship/CodeWars/GreedIsGood/src/Services/Kata.cs
@@ -0,0 +1,24 @@
+﻿namespace src.Services;
+
+public static class Kata
+{
+    public static int Score(int[] dice) {
+
+        if (dice.Count(dice => dice == 1) == 1)
+        {
+            return 100;
+        }    
+        
+        if (dice.Count(dice => dice == 5) == 1)
+        {
+            return 50;
+        }        
+        
+        if (dice.Count(dice => dice == 1) == 2)
+        {
+            return 200;
+        }
+        // Fill me in!
+        return 0;
+    }
+}
--- Craftsmanship/CodeWars/GreedIsGood/tests/GreedIsGoodShould.cs
+++ Craftsmanship/CodeWars/GreedIsGood/tests/GreedIsGoodShould.cs
@@ -1,8 +1,19 @@
+using FluentAssertions;
+using src.Services;
+
 public class GreedIsGoodShould
 {
-    [Fact]
-    public void Return_When_Given()
+    [Theory]
+    [InlineData(2,2,2,2,2,0)]
+    [InlineData(1,2,2,2,2,100)]
+    [InlineData(1,1,2,2,2,200)]
+    [InlineData(5,2,2,2,2,50)]
+    public void ReturnCorrectScor_WhenScoreIsCalculated_GivenFiveValidDice(int diceOne, int diceTwo, int diceThree, int diceFour, int diceFive, int expectedScore)
     {
-        throw new NotImplementedException();
+       var dice = new [] {diceOne, diceTwo, diceThree, diceFour, diceFive};
+       
+       var actualScore = Kata.Score(dice);
+       
+       actualScore.Should().Be(expectedScore);
     }
 }
