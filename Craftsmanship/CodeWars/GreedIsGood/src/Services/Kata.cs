95087b091c6c298e3466288c06977266c6169c70
John Fentiman <john.fentiman@capitalontap.com>
2026-07-08 13:51:43 +0100

    G

--- Craftsmanship/CodeWars/GreedIsGood/src/Services/Kata.cs
+++ Craftsmanship/CodeWars/GreedIsGood/src/Services/Kata.cs
@@ -4,19 +4,24 @@ public static class Kata
 {
     public static int Score(int[] dice) {
 
-        if (dice.Count(dice => dice == 1) == 1)
+        if (dice.Count(die => die == 1) == 1)
         {
-            return 100;
+            return 100 * dice.Count(die => die == 1);
         }    
         
-        if (dice.Count(dice => dice == 5) == 1)
+        if (dice.Count(die => die == 5) == 1)
         {
             return 50;
         }        
         
-        if (dice.Count(dice => dice == 1) == 2)
+        if (dice.Count(die => die == 1) == 2)
         {
             return 200;
+        }  
+        
+        if (dice.Count(die => die == 5) == 2)
+        {
+            return 100;
         }
         // Fill me in!
         return 0;
--- Craftsmanship/CodeWars/GreedIsGood/tests/GreedIsGoodShould.cs
+++ Craftsmanship/CodeWars/GreedIsGood/tests/GreedIsGoodShould.cs
@@ -8,6 +8,8 @@ public class GreedIsGoodShould
     [InlineData(1,2,2,2,2,100)]
     [InlineData(1,1,2,2,2,200)]
     [InlineData(5,2,2,2,2,50)]
+    [InlineData(5,5,2,2,2,100)]
+    
     public void ReturnCorrectScor_WhenScoreIsCalculated_GivenFiveValidDice(int diceOne, int diceTwo, int diceThree, int diceFour, int diceFive, int expectedScore)
     {
        var dice = new [] {diceOne, diceTwo, diceThree, diceFour, diceFive};
