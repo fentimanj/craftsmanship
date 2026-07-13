851923cae4f9382304889a3ae8460d6e6403d6f1
johnfentiman <john.fentiman@icloud.com>
2026-07-13 21:05:45 +0100

    G

--- Craftsmanship/LessonFive/TicTacToe2/src/Models/Board.cs
+++ Craftsmanship/LessonFive/TicTacToe2/src/Models/Board.cs
@@ -4,7 +4,7 @@ using Enums;
 
 internal class Board
 {
-    private readonly Dictionary<Position, Symbol> symbols = new()
+    private readonly Dictionary<Position, Symbol> moves = new()
     {
         { Position.TopRowLeftColumn, Symbol.Unknown },
         { Position.TopRowCentreColumn, Symbol.Unknown },
@@ -18,33 +18,22 @@ internal class Board
         { Position.BottomRowCentreColumn, Symbol.Unknown },
         { Position.BottomRowRightColumn, Symbol.Unknown }
     };
-
-    private Symbol topRowLeftColumn() => this.symbols[Position.TopRowLeftColumn];
-    private Symbol topRowCentreColumn() => this.symbols[Position.TopRowCentreColumn];
-    private Symbol middleRowLeftColumn() => this.symbols[Position.MiddleRowLeftColumn];
-    private Symbol middleRowRightColumn() => this.symbols[Position.MiddleRowRightColumn];
-    private Symbol middleRowCentreColumn() => this.symbols[Position.MiddleRowCentreColumn];
-    private Symbol bottomRowLeftColumn() => this.symbols[Position.BottomRowLeftColumn];
-    private Symbol bottomRowCentreColumn() => this.symbols[Position.BottomRowCentreColumn];
-    private Symbol bottomRowRightColumn() => this.symbols[Position.BottomRowRightColumn];
-
-   
-
+    
     public void AddMove(Position position, Symbol symbol)
     {
-        this.symbols[position] = symbol;
+        this.moves[position] = symbol;
     }
 
     public Symbol WinningSymbol()
     {
-        if (this.RowInLeftColumn())
+        if (this.ThreeInLeftColumn())
         {
-            return this.topRowLeftColumn();
+            return this.moves[Position.TopRowLeftColumn];
         }
 
         if (this.ThreeInCentreColumn())
         {
-            return this.topRowCentreColumn();
+            return this.moves[Position.TopRowCentreColumn];
         }
         
         return Symbol.Unknown;
@@ -52,11 +41,13 @@ internal class Board
 
     private bool ThreeInCentreColumn()
     {
-        return this.topRowCentreColumn() == this.middleRowCentreColumn() && this.topRowCentreColumn() == this.bottomRowCentreColumn();
+        return this.moves[Position.TopRowCentreColumn] == this.moves[Position.MiddleRowCentreColumn] 
+               && this.moves[Position.TopRowCentreColumn] == this.moves[Position.BottomRowCentreColumn];
     }
     
-    private bool RowInLeftColumn()
+    private bool ThreeInLeftColumn()
     {
-        return this.topRowLeftColumn() == this.middleRowLeftColumn() && this.topRowLeftColumn() == this.bottomRowLeftColumn();
+        return this.moves[Position.TopRowLeftColumn] == this.moves[Position.MiddleRowLeftColumn] 
+               && this.moves[Position.TopRowLeftColumn] == this.moves[Position.BottomRowLeftColumn];
     }
 }
