c6fe352da4a50d9bd04922190de5a0d6fdfa47cc
johnfentiman <john.fentiman@icloud.com>
2026-07-19 15:53:17 +0100

    G

--- Craftsmanship/LessonFour/GameOfLife/tests/GameOfLifeShould.cs
+++ Craftsmanship/LessonFour/GameOfLife/tests/GameOfLifeShould.cs
@@ -1,8 +1,30 @@
+using FluentAssertions;
+
 public class GameOfLifeShould
 {
     [Fact]
     public void Return_When_Given()
     {
-        throw new NotImplementedException();
+        List<Cell> seed = new List<Cell>();
+        GameOfLife gameOfLife = new GameOfLife(seed);
+        bool continueGenerating = gameOfLife.ContinueGenerating();
+        continueGenerating.Should().BeFalse();
     }
+}
+
+public class GameOfLife
+{
+    public GameOfLife(List<Cell> seed)
+    {
+       
+    }
+
+    public bool ContinueGenerating()
+    {
+        return false;
+    }
+}
+
+public class Cell
+{
 }
