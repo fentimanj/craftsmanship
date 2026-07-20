0fc5f48f1b43c6561aab20c750df722e49cacfa6
johnfentiman <john.fentiman@icloud.com>
2026-07-20 12:31:25 +0100

    R

--- Craftsmanship/LessonFour/GameOfLife/src/Models/Cells.cs
+++ Craftsmanship/LessonFour/GameOfLife/src/Models/Cells.cs
@@ -12,13 +12,13 @@ public class Cells(List<Cell> cells)
         if (cells.Count == 3)
         {
             cells.RemoveAt(0);
-            cells.RemoveAt(0);
+            cells.RemoveAt(1);
             return;
         }
         
         if (cells.Count == 2)
         {
-            cells.RemoveAt(0);
+            cells.RemoveAt(1);
         }
         
         if (cells.Count == 1)
