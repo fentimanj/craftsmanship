a4448f564e2ab295798c867ad89cce30d01bb43c
johnfentiman <john.fentiman@icloud.com>
2026-07-20 08:28:16 +0100

    R

--- Craftsmanship/LessonFour/GameOfLife/src/Services/GameOfLife.cs
+++ Craftsmanship/LessonFour/GameOfLife/src/Services/GameOfLife.cs
@@ -2,27 +2,15 @@ namespace src.Services;
 
 using Models;
 
-public sealed class GameOfLife
+public sealed class GameOfLife(Cells cells)
 {
-    private Cells seed;
-
-    public GameOfLife(Cells seed)
-    {
-        this.seed = seed;
-    }
-
-    public bool ContinueGenerating()
-    {
-        return this.seed.HasLivingCells();
-    }
-
     public int GetNumberOfLivingCells()
     {
-        return this.seed.GetLivingCells();
+        return cells.GetLivingCells();
     }
 
     public void CompleteLifecycle()
     {
-        this.seed.KillCell();
+        cells.KillCell();
     }
 }
