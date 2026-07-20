a4445c761503e67ea69abf9cf3a99f494c2c7672
johnfentiman <john.fentiman@icloud.com>
2026-07-20 18:55:20 +0100

    Rollback 1

--- Craftsmanship/LessonSix/EMCExample/tests/EMCExampleShould.cs
+++ Craftsmanship/LessonSix/EMCExample/tests/EMCExampleShould.cs
@@ -6,10 +6,8 @@ public class EMCExampleShould
     public void Return_When_Given()
     {
         var emcService = new EmcService();
-
-        var partOne = 1.ToString();
-        var partTwo = 2.ToString();
-        var result = emcService.Add(partOne, partTwo);
+        
+        var result = emcService.AddOld(1, 2);
 
         result.Should().Be("12");
     }
@@ -17,6 +15,13 @@ public class EMCExampleShould
 
 public class EmcService
 {
+    public string AddOld(int i, int i1)
+    {
+        var partOne = i.ToString();
+        var partTwo = i1.ToString();
+        return this.Add(partOne, partTwo);
+    }
+
     public string Add(string partOne, string partTwo)
     {
         return partOne + partTwo;
