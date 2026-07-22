582373668c7ace7a90793ee0684303a9da6dfeb5
John Fentiman <john.fentiman@capitalontap.com>
2026-07-22 08:52:15 +0100

    R

--- Craftsmanship/CodeWars/GreedIsGood/tests/GreedIsGoodShould.cs
+++ Craftsmanship/CodeWars/GreedIsGood/tests/GreedIsGoodShould.cs
@@ -10,7 +10,7 @@ public class GreedIsGoodShould
     [InlineData(5,2,2,2,2,50)]
     [InlineData(5,5,2,2,2,100)]
     
-    public void ReturnCorrectScor_WhenScoreIsCalculated_GivenFiveValidDice(int diceOne, int diceTwo, int diceThree, int diceFour, int diceFive, int expectedScore)
+    public void ReturnCorrectScore_WhenScoreIsCalculated_GivenFiveValidDice(int diceOne, int diceTwo, int diceThree, int diceFour, int diceFive, int expectedScore)
     {
        var dice = new [] {diceOne, diceTwo, diceThree, diceFour, diceFive};
        
