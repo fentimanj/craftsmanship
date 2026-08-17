6df4cb9d0d7cf53acf8ae25d4a6de257d32074fb
johnfentiman <john.fentiman@icloud.com>
2026-08-17 18:59:25 +0100

    G

--- Craftsmanship/LessonFour/GameOfLife/src/Models/Cells.cs
+++ Craftsmanship/LessonFour/GameOfLife/src/Models/Cells.cs
@@ -4,96 +4,36 @@ public class Cells(List<Cell> cells)
 {
     public void KillCell()
     {
-        if (cells.Count == 0)
+        var deadCells = new List<Cell>();
+
+        if (cells.Count < 1)
         {
             return;
         }
 
-        if (cells.Count == 3)
+
+        foreach (var cell in cells)
         {
-            var cellInColumnOne = this.CellInColumn(1);
-            var cellInColumnTwo = this.CellInColumn(2);
-            var cellInColumnThree = this.CellInColumn(3);
-            var cellInColumnFour = this.CellInColumn(4);
-
-            var killCellInColumnOne = false;
-            var killCellInColumnTwo = false;
-            var killCellInColumnThree = false;
-            var killCellInColumnFour = false;
-
-            var cellToTheRightOfCellInColumnOne = this.GetCellToRight(cellInColumnOne);
-            var cellToTheLeftOfCellInColumnOne = this.GetCellToLeft(cellInColumnOne);
-
-            if (cellToTheRightOfCellInColumnOne == null || cellToTheLeftOfCellInColumnOne == null)
-            {
-                killCellInColumnOne = true;
-            }
-
-            var cellToTheRightOfCellInColumnTwo = this.GetCellToRight(cellInColumnTwo);
-            var cellToTheLeftOfCellInColumnTwo = this.GetCellToLeft(cellInColumnTwo);
-
-            if (cellToTheRightOfCellInColumnTwo == null || cellToTheLeftOfCellInColumnTwo == null)
-            {
-                killCellInColumnTwo = true;
-            }
-
-            var cellToTheRightOfCellInColumnThree = this.GetCellToRight(cellInColumnThree);
-            var cellToTheLeftOfCellInColumnThree = this.GetCellToLeft(cellInColumnThree);
-
-            if (cellToTheRightOfCellInColumnThree == null || cellToTheLeftOfCellInColumnThree == null)
-            {
-                killCellInColumnThree = true;
-            }
-
-            var cellToTheRightOfCellInColumnFour = this.GetCellToRight(cellInColumnFour);
-            var cellToTheLeftOfCellInColumnFour = this.GetCellToLeft(cellInColumnFour);
-            
-            if(cellToTheRightOfCellInColumnFour == null || cellToTheLeftOfCellInColumnFour == null)
-            {
-                killCellInColumnFour = true;
-            }
-
-            if (killCellInColumnOne)
-            {
-                cells.Remove(cellInColumnOne);
-            }
-
-            if (killCellInColumnTwo)
-            {
-                cells.Remove(cellInColumnTwo);
-            }
-
-            if (killCellInColumnThree)
-            {
-                cells.Remove(cellInColumnThree);
-            }
-
-            if (killCellInColumnFour)
-            {
-                cells.Remove(cellInColumnFour);
-            }
-            
-            return;
+            this.IdentifyDeadCells(cell, deadCells);
         }
 
-        if (cells.Count == 2)
+        foreach (var cell in deadCells)
         {
-            var cellInColumnOne = this.CellInColumn(1);
-            var cellInColumnTwo = this.CellInColumn(2);
-            var cellInColumnThree = this.CellInColumn(3);
-
-            var cellOne = cells[0];
-            var cellTwo = cells[1];
-            cells.Remove(cellOne);
-            cells.Remove(cellTwo);
-            return;
+            cells.Remove(cell);
         }
+    }
 
-        if (cells.Count == 1)
+    private void IdentifyDeadCells(Cell cell, List<Cell> deadCells)
+    {
+        if (this.HasNeighbours(cell))
         {
-            var cellOne = cells[0];
-            cells.Remove(cellOne);
+            deadCells.Add(cell);
         }
+    }
+
+    private bool HasNeighbours(Cell cell)
+    {
+        return this.GetCellToLeft(cell) == null || GetCellToRight(cell, cells) == null;
     }
 
     private Cell? GetCellToLeft(Cell? cellInColumnOne)
@@ -101,14 +41,9 @@ public class Cells(List<Cell> cells)
         return cells.FirstOrDefault(cell => cellInColumnOne != null && cellInColumnOne.HasCellToLeft(cell));
     }
 
-    private Cell? GetCellToRight(Cell? cellInColumnOne)
+    private static Cell? GetCellToRight(Cell? cellInColumnOne, List<Cell> cells)
     {
         return cells.FirstOrDefault(cell => cellInColumnOne != null && cellInColumnOne.HasCellToRight(cell));
-    }
-
-    private Cell? CellInColumn(int columnIndex)
-    {
-        return cells.FirstOrDefault(cell => cell.GetColumnIndex() == columnIndex);
     }
 
     public int GetLivingCells()
