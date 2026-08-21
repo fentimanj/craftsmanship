9f6c4cbb25ee610b8ad15a102071ab91cf3cdb8d
John Fentiman <john.fentiman@capitalontap.com>
2026-08-21 14:38:13 +0100

    R

---
+++ Craftsmanship/LessonFour/GameOfLife/src/Models/CellExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace src.Models;
+
+public static class CellExtensions
+{
+    private static Cell? GetCellToLeft(this Cell? thisCell, List<Cell> cells)
+    {
+        return cells.FirstOrDefault(cell => thisCell != null && thisCell.HasCellToLeft(cell));
+    }
+
+    private static Cell? GetCellToRight(this Cell? cellInColumnOne, List<Cell> allCells)
+    {
+        return allCells.FirstOrDefault(cell => cellInColumnOne != null && cellInColumnOne.HasCellToRight(cell));
+    }
+
+    public static bool HasNeighbour(this Cell cell, List<Cell> cells)
+    {
+        var cellToRight = cell.GetCellToRight(cells);
+        var cellToLeft = cell.GetCellToLeft(cells);
+
+        if (cellToLeft != null && cellToRight != null) return true;
+        return false;
+    }
+}
--- Craftsmanship/LessonFour/GameOfLife/src/Models/Cells.cs
+++ Craftsmanship/LessonFour/GameOfLife/src/Models/Cells.cs
@@ -1,6 +1,6 @@
 namespace src.Models;
 
-public class Cells(List<Cell> cells)
+public sealed class Cells(List<Cell> cells)
 {
     public void ClearDeadCells()
     {
@@ -13,27 +13,9 @@ public class Cells(List<Cell> cells)
 
     private void AddDeadCellsToKillList(Cell cell, List<Cell> killList)
     {
-        var cellToRight = GetCellToRight(cell);
-        var cellToLeft = GetCellToLeft(cell);
-
-        if (cellToLeft != null && cellToRight != null) return;
+        if (cell.HasNeighbour(cells)) return;
 
         killList.Add(cell);
-    }
-
-    private Cell? GetCellToLeft(Cell? thisCell)
-    {
-        return cells.FirstOrDefault(cell => thisCell != null && thisCell.HasCellToLeft(cell));
-    }
-
-    private Cell? GetCellToRight(Cell? cellInColumnOne)
-    {
-        return cells.FirstOrDefault(cell => cellInColumnOne != null && cellInColumnOne.HasCellToRight(cell));
-    }
-
-    private Cell? CellInColumn(int columnIndex)
-    {
-        return cells.FirstOrDefault(cell => cell.GetColumnIndex() == columnIndex);
     }
 
     public int GetLivingCells()
